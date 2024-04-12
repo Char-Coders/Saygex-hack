a9b96e0c8fb5367eed11ef6bfaaddc81904025f1
LakshayGMZ <lakshaygulati1898@gmail.com>
2024-04-12 16:44:13 +0530

    Update

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+
+import flask
+import flask_cors
 from chatbot import Inference
 
 
 chatbot = Inference()
+chatbot_doc = Inference(True)
+app = flask.Flask(__name__)
+flask_cors.CORS(app)
 
-def chat(text) :
+@app.route('/', methods=['GET'])
+def uwu():
+    return 'Pani Peele Bhai'
+
+@app.route('/reset')
+def reset():
+    chatbot = Inference()
+
+@app.route('/chatUser')
+def chatUser():
+    text = flask.request.args['text']
+    resp = chatbot_doc.run(text)
+    return {'content': resp['content']}
+
+@app.route('/chatDoctor')
+def chatDoctor():
+    text = flask.request.args['text']
     resp = chatbot.run(text)
-    return resp
-        
-def main():
-    while True:
-        ask = input("> ")
-        print(chat(ask))
+    return {'content': resp['content']}
+
+# def main():
+#     while True:
+#         ask = input("> ")
+#         print(chat(ask))
 
 if __name__ == "__main__":
-    main()
+    app.run()
