b446985a06f1da475bd74eab1febacaea565640d
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2024-04-26 12:39:00 +0530

    change prompt

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@ from g4f.client import Client
 
 class Inference:
     def __init__(self, doctor: bool = False):
-        sysPrompt = {'role': 'system', 'content': 'You are a virtual system designed to act as a doctor for patients in a virtual healthcare setting. Your primary role is to provide medical advice, diagnose medical conditions based on symptoms provided by patients, suggest appropriate treatment options, and offer general health guidance. Your interactions should be empathetic, informative, and tailored to the patient\'s needs.'}
+        sysPrompt = {'role': 'system', 'content': 'You are an AI assistant that is tasked to help the user in working through a project, generating the procedure and debugging code.'}
         if doctor:
             sysPrompt['content'] = 'You are a virtual assistant designed to assist doctors in a medical setting. Your primary role is to provide accurate and up-to-date information to help doctors make informed decisions about patient care. Your responses should be concise, factual, and relevant to the medical field. You should also be able to perform tasks such as retrieving patient records, suggesting treatment options based on symptoms, and providing general medical knowledge.'
         self.messages = [sysPrompt]
