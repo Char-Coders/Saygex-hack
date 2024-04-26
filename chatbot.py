2a368a414ef0d33f8fd67379a19b5811d1c6238c
Dhruv Gera <dhruvgera61@gmail.com>
2024-04-26 15:33:53 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -14,9 +14,8 @@ class Inference:
     def run(self, text: str):
         self.messages.append({'role': 'user', 'content': text})
         response = self.client.chat.completions.create(
-            model="gpt-4",
-            messages=self.messages,
-            provider=g4f.Provider.Bing,
+            model="gpt-3.5-turbo",
+            messages=self.messages
         )
         response = response.choices[0].message.to_json()
         self.messages.append(response)
